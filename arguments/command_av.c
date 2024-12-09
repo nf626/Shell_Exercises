183fa2f9353457a59df896c54624c828b5fa4dfd
Nigel Feng <10083@holbertonstudents.com>
2024-12-09 09:18:26 +0000

    2. command line to av

---
+++ arguments/command_av.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that splits a string and returns an array of each word of the string.
+ *
+ * Return: 0, Success.
+ */
+int main()
+{
+  
+  return (0);
+}
