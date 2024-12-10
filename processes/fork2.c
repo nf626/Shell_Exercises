9d0c0e8b3d82f481285320fa75d6fbf246dfa38c
Nigel Feng <10083@holbertonstudents.com>
2024-12-10 13:27:13 +0000

    Creating processes

--- processes/fork2.c
+++ processes/fork2.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
 /**
- * main - fork & wait example
+ * main - fork example
  *
  * Return: Always 0.
  */
 int main(void)
 {
+    pid_t my_pid;
     pid_t child_pid;
-    int status;
 
     child_pid = fork();
     if (child_pid == -1)
@@ -19,15 +17,15 @@ int main(void)
         perror("Error:");
         return (1);
     }
+    my_pid = getpid();
+    printf("My pid is %u\n", my_pid);
     if (child_pid == 0)
     {
-        printf("Wait for me, wait for me\n");
-        sleep(3);
+        printf("(%u) Nooooooooo!\n", my_pid);
     }
     else
     {
-        wait(&status);
-        printf("Oh, it's all better now\n");
+        printf("(%u) %u, I am your father\n", my_pid, child_pid);
     }
     return (0);
 }
