19f6046be2f12897dabf72807589ef173e4008dd
Nigel Feng <10083@holbertonstudents.com>
2024-12-12 02:23:30 +0000

    fork, wait, execve

--- fork_wait_execve.c
+++ fork_wait_execve.c
@@ -14,32 +14,46 @@
  */
 int main()
 {
-  pid_t child_pid;
+  pid_t pid, init_pid;
   int status;
+  int count = 5, i = 0;
   char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
   char *envp[] = {NULL};
 
-  printf("Execute program\n");
+  printf("Execve program\n");
   printf("=================================================\n");
+  init_pid = getppid();
+  printf("init pid: %u\n", init_pid);
 
-  child_pid = fork();
-
-  if (child_pid == -1)
+  while (i < count)
     {
-      perror("Error:");
-      exit(1);
-    }
-  if (child_pid == 0)
-    {
-      if (execve(argv[0], argv, envp) == -1)
+      pid = fork();
+      if (pid == -1)
 	{
 	  perror("Error:");
+	  exit(-1);
 	}
+    	if (pid == 0)
+	{
+	  printf("Child process: PPID = %u, PID = %u\n", getppid(), getpid());
+	  if (execve(argv[0], argv, envp) == -1)
+	    {
+	      perror("Error:");
+	    }
+	  execve(argv[0], argv, envp);
+	  exit(0);
+	}
+      else
+	{
+	  printf("Parent process: PID = %u\n", getpid());
+	  printf("Waiting for Child processes to finish...\n");
+	  wait(&status);
+	  printf("Child process is finished.\n");
+	}
+      i = i + 1;
     }
-  else
-    {
-      wait(&status);
-      printf("Done with execve.\n");
-    }
+     
+  printf("Execve has finished.\n");
+  
   return (0);
 }
