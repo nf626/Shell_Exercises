c4d07836d1ca6b00f4c9647d85e0780ae741d299
Nigel Feng <10083@holbertonstudents.com>
2024-12-13 12:30:12 +0000

    5. setenv

--- environment/5.setenv.c
+++ environment/5.setenv.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern char **environ;
 /**
  * _setenv - changes or adds an environment variable
  *           (without using setenv).
- * @name:
+ * @name: adds the variable name to the environment.
  * @value:
  * @overwrite:
  *
@@ -12,23 +13,38 @@
  */
 int _setenv(const char *name, const char *value, int overwrite)
 {
-  putenv("name");
-  if ()
+  char *copy = malloc(sizeof(char *));
+  if (copy == NULL)
     {
+      free(copy);
       return (-1);
     }
-  return (0);
+  if (getenv(name) == 1)
+    {
+      if (overwrite == 0)
+	{
+	}
+      else if (overwrite > 0)
+	{
+	  replace value
+	}
+      return (0);
+    }
+  else
+    {
+      perror("Error:");
+      return (-1);
+    }
 }
 
 int main()
 {
-  char *env_var = getenv("TEST");
-  printf("%s\n", env_var);
+  char newvar[] = "language";
+  char value[] = "C";
   
-  _setenv("TEST", "Variable has changed", 0);
-  setenv("TEST2", "Changed", 1);
-
-  char *new = getenv("TEST2");
-  printf("%s\n", new);
+  _setenv(newvar, value, 1);
+  
+  printf("Result: %s\n", getenv(newvar));
+  
   return (0);
 }
