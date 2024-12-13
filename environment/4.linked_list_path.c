df0aadd5d6b68feaeb8eabd5e062bf4661a96b02
Nigel Feng <10083@holbertonstudents.com>
2024-12-13 06:02:08 +0000

    linked list of the PATH directories.

--- environment/4.linked_list_path.c
+++ environment/4.linked_list_path.c
@@ -3,24 +3,88 @@
 #include <unistd.h>
 #include <string.h>
 
-typedef struct path
-{
+typedef struct path {
   char *str;
   struct path *next;
 } path_t;
 
+void print_list(const path_t *h)
+{
+  while (h != NULL)
+    {
+      if (h->str == NULL)
+	{
+	  printf("[0] (nil)\n");
+	  h = h->next;
+	}
+      else
+	{
+	  printf("%s\n", h->str);
+	  h = h->next;
+	}
+    }
+}
+
+path_t *add_node_end(path_t **head, const char *str)
+{
+  path_t *current = NULL;
+  path_t *last = NULL;
+  
+  last = malloc(sizeof(path_t));
+  if (last == NULL)
+    {
+      free(last);
+      return (NULL);
+    }
+  last->str = strdup(str);
+  last->next = NULL;
+  if (*head == NULL)
+    {
+      *head = last;
+      return (last);
+    }
+  else
+    {
+      current = *head;
+      while (current->next != NULL)
+	{
+	  current = current->next;
+	}
+      current->next = last;
+    }
+  return (current);
+}
+
+void free_list(path_t *head)
+{
+  path_t *temp = NULL;
+
+  while (head != NULL)
+    {
+      temp = head;
+      head = head->next;
+      free(temp->str);
+      free(temp);
+    }
+  free(head);
+}
+
 int main()
 {
   unsigned int i = 0;
-  char *pathname;
-  char *token;
+  char *pathname, *token;
+  path_t *head = NULL;
   
   pathname = getenv("PATH");
   token = strtok(pathname, ":");
-
+ 
   while (token != NULL)
     {
-      printf("%s", token); 
+      add_node_end(&head, token);
       token = strtok(NULL, ":");
     }
+  print_list(head);
+  free_list(head);
+  head = NULL;
+  return (0);
 }
