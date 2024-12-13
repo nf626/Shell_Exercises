b5e501c8fb3d23acc4c6b26c5e842750942cedc5
Nigel Feng <10083@holbertonstudents.com>
2024-12-13 06:25:28 +0000

    5. setenv

---
+++ environment/5.setenv.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _setenv - changes or adds an environment variable
+ *           (without using setenv).
+ * @name:
+ * @value:
+ * @overwrite:
+ *
+ * Return: zero on success, or -1 on error.
+ */
+int _setenv(const char *name, const char *value, int overwrite)
+{
+}
+
+int main()
+{
+  return (0);
+}
