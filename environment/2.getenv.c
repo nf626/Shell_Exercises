719a32fe9bdecdf9621db5770a8bb3a11bb83de6
Nigel Feng <10083@holbertonstudents.com>
2024-12-13 01:16:19 +0000

    2. getenv

--- environment/2.getenv.c
+++ environment/2.getenv.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+extern char **environ;
+
+/**
+ * _getenv - Function searches the environment list to find the
+ *           environment variable name, and returns a pointer to the
+ *           corresponding value string.
+ * @name: Environment variable name.
+ *
+ * Return: a pointer to the value in the
+ *         environment, or NULL if there is no match.
+ */
 char *_getenv(const char *name)
 {
+  unsigned int i = 0;
+  char *token;
   
+  while(environ[i] != NULL)
+    {
+      token = strtok(environ[i], "=");
+      if (strcmp(token, name) == 0)
+	{
+	  token = strtok(NULL, "=");
+	  return (token);
+	}
+      i++;
+    }
+  return (NULL);
+}
+
+/**
+ * main - Prints out environment list.
+ *
+ * Return: 0, Success.
+ */
+void main()
+{
+  char *name;
+
+  name = _getenv("PATH");
+  
+  printf("Environment: %s\n", name);
 }
---
+++ environment/3.print_path.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * prints each directory contained in the the environment variable PATH, one directory per line.
+ *
+ */
+int main()
+{
+  
+}
