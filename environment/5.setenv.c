0d067db67b854b9f556495f3f2f21123757164cb
Nigel Feng <10083@holbertonstudents.com>
2024-12-14 07:37:13 +0000

    5. setenv

--- environment/5.setenv.c
+++ environment/5.setenv.c
@@ -11,24 +11,28 @@
  */
 int _setenv(const char *name, const char *value, int overwrite)
 {
-  char *env_var = _getenv(name);
+  char *new_env;
+  char **copy_env = environ;
+  size_t i, j;
+ /** Make new environment variable with format NANME=Value */
+  size_t name_length = strlen(name), value_length = strlen(value);
+  size_t env_length = name_length + value_length + 2;
 
-  if (name == NULL || value == NULL || overwrite == NULL)
+  if (name == NULL || value == NULL)
     {
       perror("Error:");
       return (-1);
     }
-  
-  if (name is there);
+
+  new_env = malloc(env_length);
+  if (new_env == NULL)
     {
-      if (overwrite == 0)
-	{
-	  value of name is not changed;
-	  return (0);
-	}
-      else
-	{
-	  print_list(name);
-	  return (0);
-	}
+      free(new_env);
+      return (-1);
+    }
+  
+  while ((*copy_env) != NULL)
+    {
+      if (strcmp()
+    }
 }
